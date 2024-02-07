cf979eb70c8df25b14304c89c84a449429723eb5
Tobias Schoch <tobias.schoch@gmail.com>
2024-02-07 20:45:42 +0100

    Update tests

--- tests/test_wbacon.R
+++ tests/test_wbacon.R
@@ -1,81 +1,70 @@
 #===============================================================================
 # SUBJECT  Test the implementation of 'wbacon' against the function BACON in the
 #          package 'robustX'
-# AUTHORS  Tobias Schoch, tobias.schoch@gmail.com, January 23, 2021
+# AUTHORS  Tobias Schoch, tobias.schoch@gmail.com, Feburary 7, 2024
 # LICENSE  GPL >= 2
-# COMMENT  pkg 'robustbase' must be installed
+# COMMENT  pkg 'robustX' is required
 #===============================================================================
 library(wbacon)
-library(robustX)
 
-#===============================================================================
-# Comparison function
-#===============================================================================
-# this function compares our implementation with the reference implementation
-# in pkg 'robustX'
-compare <- function(data, name, init = "V2"){
-	# we test 11 different values for the parameter 'alpha'
-	set_of_alphas <- c(seq(0.3, 0.9, 0.1), 0.95, 0.99, 0.999, 0.9999)
-	data <- as.matrix(data)
+# pkg robustX is required
+if (requireNamespace("robustX", quietly = TRUE)) {
+    library(robustX)
 
-	res <- NULL
-	acc <- sqrt(.Machine$double.eps)
-	for (i in 1:length(set_of_alphas)) {
+    # utility function
+    all_equal <- function(target, current, label,
+        tolerance = sqrt(.Machine$double.eps), scale = NULL,
+        check.attributes = FALSE)
+    {
+        if (missing(label))
+            stop("Argument 'label' is missing\n")
+        res <- all.equal(target, current, tolerance, scale,
+            check.attributes = check.attributes)
+        if (is.character(res))
+            cat(paste0(label, ": ", res, "\n"))
+    }
 
-		# reference implementation robustX::BACON with the "V2" (version 2)
-		# intialization method of Billor et al. (2000)
-		ref.init <- ifelse(init == "V2", "V2", "Mahalanobis")
-		reference <- BACON(data, alpha = set_of_alphas[i], init.sel = ref.init,
-			verbose = FALSE)
+    #---------------------------------------------------------------------------
+    # Comparison function
+    #---------------------------------------------------------------------------
+    # this function compares our implementation with the reference
+    # implementation in pkg 'robustX'
+    compare <- function(data, name, init = "V2"){
+        # we test 11 different values for the parameter 'alpha'
+        set_of_alphas <- c(seq(0.3, 0.9, 0.1), 0.95, 0.99, 0.999, 0.9999)
+        data <- as.matrix(data)
 
-		# our implementation
-		new_imp <- wBACON(data, alpha = set_of_alphas[i], version = init)
-		deviations <- NULL
+        for (i in 1:length(set_of_alphas)) {
+            # reference implementation robustX::BACON with the "V2" (version 2)
+            # intialization method of Billor et al. (2000)
+            ref.init <- ifelse(init == "V2", "V2", "Mahalanobis")
+            reference <- suppressWarnings({
+                mvBACON(data, alpha = set_of_alphas[i], init.sel = ref.init,
+                        verbose = FALSE)
+            })
 
-		# absolute deviations
-		dev_location <- max(abs(reference$center - new_imp$center))
-		dev_scatter <- max(abs(reference$cov - new_imp$cov))
-		dev_distance <- max(abs(reference$dis - new_imp$dist))
+            # our implementation
+            new_imp <- wBACON(data, alpha = set_of_alphas[i], version = init)
 
-		# deviations in terms of the selected subset
-		dev_subset <- sum(reference$subset - new_imp$subset)
+            # comparison
+            meth <- paste0("data = ", name, "init" = init, ", ",
+                           "alpha = ", set_of_alphas[i])
+            all_equal(reference$center, new_imp$center, paste0("center: ", meth))
+            all_equal(reference$cov, new_imp$cov, paste0("cov: ", meth))
+            all_equal(reference$dis, new_imp$dist, paste0("dist: ", meth))
+            all_equal(reference$subset, new_imp$subset == 1,
+                      paste0("subset: ", meth))
+        }
+    }
 
-		at <- matrix(rep(0, 4), ncol = 4)
-		rownames(at) <- paste(set_of_alphas[i])
-
-		# throw an error if the deviations are larger than 'acc'
-		if (dev_location > acc)
-			at[1] <- dev_location
-		if (dev_scatter > acc)
-			at[2] <- dev_scatter
-		if (dev_distance> acc)
-			at[3] <- dev_distance
-		if (dev_subset> acc)
-			at[4] <- dev_subset
-
-		if (any(at > 0))
-		res <- rbind(res, at)
-	}
-
-	if (!is.null(res)) {
-		cat(name, ": differences detected\n")
-		colnames(res) <- c("center", "cov", "distance", "subset")
-        print(res)
-	}
-
-    return(NROW(res))
-}
-
-#===============================================================================
-# Tests I
-#===============================================================================
-# We test the implementations on 7 well known data sets.
-errors <- 0
-
-setup <- matrix(c(
-#------------------------------
-#    DATASET        VARIABLES
-#------------------------------
+    #---------------------------------------------------------------------------
+    # Tests
+    #---------------------------------------------------------------------------
+    # We test the implementations on 7 well known data sets.
+    setup <- matrix(c(
+    #------------------------------
+    #    DATASET        VARIABLES
+    #------------------------------
     "hbk",          "1:3",
     "bushfire",     "1:5",
     "aircraft",     "1:4",
@@ -84,20 +73,18 @@ setup <- matrix(c(
     "milk",         "1:8",
     "pulpfiber",    "1:8"), byrow = TRUE, ncol = 2)
 
-# check that version 1.25 (or newer) of robustX is installed
-robustX_version <- as.numeric(gsub("-", "", getNamespaceVersion("robustX")))
-if (robustX_version >= 1.25) {
-    for (i in 1:nrow(setup)) {
-        data_name <- setup[i, 1]
-        data(list = data_name, package = "robustbase")
-        dt <- data.matrix(get(data_name))
-        eval(parse(text = paste0("dt <- dt[,", setup[i, 2], "]")))
-        errors <- errors + compare(dt, data_name, "V1")
-        errors <- errors + compare(dt, data_name, "V2")
+    robustX_version <-
+        as.numeric(gsub("-", "", getNamespaceVersion("robustX")))
+
+    # check that version 1.25 (or newer) of robustX is installed
+    if (robustX_version >= 1.25) {
+        for (i in 1:nrow(setup)) {
+            data_name <- setup[i, 1]
+            data(list = data_name, package = "robustbase")
+            dt <- data.matrix(get(data_name))
+            eval(parse(text = paste0("dt <- dt[,", setup[i, 2], "]")))
+            compare(dt, data_name, "V1")
+            compare(dt, data_name, "V2")
+        }
     }
-    if (errors == 0)
-        cat("\nno errors\n\n")
-} else {
-    cat("Version >= 1.25 of package 'robustX' is required, you have",
-        robustX_version, "\n")
 }
--- tests/test_wbacon_reg.R
+++ tests/test_wbacon_reg.R
@@ -2,60 +2,72 @@
 # SUBJECT  Test the implementation of 'wBACON_reg'
 # AUTHORS  Tobias Schoch, tobias.schoch@gmail.com
 # LICENSE  GPL >= 2
-# COMMENT  pkg 'robustbase' must be installed
+# COMMENT  pkg 'robustbase' and 'robustX' must be installed
 #===============================================================================
 library(wbacon)
-library(robustX)
 
-#===============================================================================
-# Tests
-#===============================================================================
-# We test our implementation against the method robustX::BACON for 5 well known
-# data sets.
+if (requireNamespace("robustX", quietly = TRUE) &
+    requireNamespace("robustbase", quietly = TRUE)) {
 
-compare <- function(formula, data, alpha = 0.05, original = TRUE,
-    verbose = FALSE)
-{
-    m <- wBACON_reg(formula, data = data, alpha = alpha, original = original,
-        verbose = verbose)
-	y <- as.numeric(model.response(m$model))
-	x <- model.matrix(m$terms, m$model)
-    ref <- suppressWarnings(BACON(x[, -1], y, init.sel = "V2", alpha = alpha,
-        verbose = verbose))
-    res <- sum(xor(m$subset, unname(ref$subset)))
-    call <- match.call()
-    if (res > 0)
-        cat(paste0("\n The subsets differ in ", res, " places for '",
-            call$data, "'\n"))
-    res
-}
+    library(robustX)
+    library(robustbase)
 
-errors <- 0
-
-# check that version 1.25 (or newer) of robustX is installed
-robustX_version <- as.numeric(gsub("-", "", getNamespaceVersion("robustX")))
-if (robustX_version >= 1.25) {
-    data(hbk, package = "robustbase")
-    errors <- errors + compare(Y ~ ., hbk)
-
-    data(aircraft, package = "robustbase")
-    errors <- errors + compare(Y ~ ., data = aircraft)
-
-    data(education, package = "robustbase")
-    errors <- errors + compare(Y ~ Region + X1 + X2 + X2, data = education)
-
-    data(heart, package = "robustbase")
-    errors <- errors + compare(clength ~ ., data = heart)
-
-    data(pulpfiber, package = "robustbase")
-    errors <- errors + compare(Y1 ~ X1 + X2 + X3, data = pulpfiber)
-
-    if (errors == 0) {
-        cat("\nno errors\n\n")
-    } else {
-        cat("\n")
+    #---------------------------------------------------------------------------
+    # function to compare wBACON_reg against BACON
+    #---------------------------------------------------------------------------
+    all_equal <- function(target, current, label,
+        tolerance = sqrt(.Machine$double.eps), scale = NULL,
+        check.attributes = FALSE)
+    {
+        if (missing(label))
+            stop("Argument 'label' is missing\n")
+        res <- all.equal(target, current, tolerance, scale,
+            check.attributes = check.attributes)
+        if (is.character(res))
+            cat(paste0(label, ": ", res, "\n"))
     }
-} else {
-    cat("Version >= 1.25 of package 'robustX' is required, you have",
-        robustX_version, "\n")
+    compare <- function(formula, data, name, alpha = 0.05, original = TRUE,
+        verbose = FALSE)
+    {
+        # our implementation
+        m <- wBACON_reg(formula, data = data, alpha = alpha,
+                        original = original, verbose = verbose)
+
+        # we extract the response variable and the design matrix
+        y <- as.numeric(model.response(m$model))
+        x <- model.matrix(m$terms, m$model)
+
+        # reference implementation (robustX)
+        ref <- suppressWarnings({
+            BACON(x[, -1], y, init.sel = "V2", alpha = alpha,
+                  verbose = verbose)
+        })
+
+        all_equal(m$subset, ref$subset, name)
+    }
+
+    # check that version 1.25 (or newer) of robustX is installed
+    robustX_version <-
+        as.numeric(gsub("-", "", getNamespaceVersion("robustX")))
+    #---------------------------------------------------------------------------
+    # Tests
+    #---------------------------------------------------------------------------
+    # We test our implementation against the method robustX::BACON for 5 well
+    # known data sets.
+    if (robustX_version >= 1.25) {
+        data(hbk, package = "robustbase")
+        compare(Y ~ ., hbk, "hbk")
+
+        data(aircraft, package = "robustbase")
+        compare(Y ~ ., aircraft, "aircraft")
+
+        data(education, package = "robustbase")
+        compare(Y ~ Region + X1 + X2 + X2, education, "education")
+
+        data(heart, package = "robustbase")
+        compare(clength ~ ., heart, "heart")
+
+        data(pulpfiber, package = "robustbase")
+        compare(Y1 ~ X1 + X2 + X3, pulpfiber, "pulpfiber")
+    }
 }
